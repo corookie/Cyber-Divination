d1f9148ba303fd65cf23b8b9424e3784e817b9b5
corookie <18945118328@163.com>
2023-08-26 17:52:41 +0800

    初次修改

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import 'lib-flexible/flexible'
+import Vue from 'vue'
+import router  from './router'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createApp } from 'vue';
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(router)
+.use(ElementPlus, { size: 'small', zIndex: 3000 })
+.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import vue from '@vitejs/plugin-vue'
+// vite.config.ts
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineConfig({
-  plugins: [vue()],
+  server :{
+    port:8080
+  },
+  // ...
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `@import "element-plus/theme-chalk/index.css";`
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
